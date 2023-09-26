2048e5798f70862ef30200106e0f73690188e25d
hfl <hfl@huangfeilong.com>
2023-09-26 15:41:08 +0800

    fix font awesome 5 to 6

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Daning Rails Admin
+大宁是一款基于 Devise/Pundit/Rolify Gem 和 Bootstrap 的 SB-admin-2
+模板构建的 Rails 后台管理系统。
+
+# 安装
+
+1. 下载；
+2. 进入系统文件夹，安装相关依赖软件；
+3. 初始化；
+4. 运行服务器。
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,6 @@ class Admin::DashboardController < ApplicationController
   before_action :authenticate_user!
   layout 'admin'
   def index
+    @page_title = "Dashboard"
   end
 end
